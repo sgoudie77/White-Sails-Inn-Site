0e368a3996127f5716643afdb7808ff2e805756a
Sean Goudie <sgoudie77@hotmail.com>
2022-04-24 04:02:14 +0700

    second site style updates

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 // Responsive Mobile Menu
-
+// Menu Toggle for Responsive Design
+function toggleMenu(){
+    const menuToggle = document.querySelector('.toggle');
+    const navigation = document.querySelector('.navigation');
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+  }
 // Gallery Modals
